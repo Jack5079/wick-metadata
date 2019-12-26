9329b607593ea7d72e78b4c6bba22a03f81ad945
Jack <29169102+Jack5079@users.noreply.github.com>
2019-12-25 21:45:46 -0500

    Refactor

--- main.mjs
+++ main.mjs
@@ -1,41 +1,41 @@
-/* global Worker */
+import Project from './wicktools.mjs'
+
 const file = document.getElementById('import')
-const worker = new Worker('worker.mjs', { type: 'module' })
+
 file.addEventListener('change', () => {
   if (document.getElementById('placeholder')) {
     document.getElementById('placeholder').remove()
   }
   const projects = [...file.files]
-  projects.forEach(blob => worker.postMessage(blob))
-  worker.addEventListener('message', event => {
-    const project = event.data
+  projects.forEach(async element => {
+    const project = await new Project(element)
     console.log(project) // Log it for later
     // Now, for displaying this data:
-
+    const holder = document.createElement('article')
+    holder.style = `
+      background: darkslategray;
+      display: inline-block;
+      padding: 1em;
+      margin: 1em;
+      border-radius: 1em;
+      border: 0.5em ${project.backgroundColor} solid; 
+    `
+    const header = document.createElement('h3')
+    header.innerText = project.name
     const info = document.createElement('p') // This will have most of the info
     info.innerText = `
-  ${project.name} is ${project.width}x${project.height} and runs at ${project.framerate} FPS
-  It was also made on ${project.metadata.platform.description}
-  The background color was ${project.backgroundColor}`
-
-    info.style.backgroundColor = 'white' // Change the website's background to match the project's background
-    info.style.color = 'black'
-
-    info.style.display = 'inline' // so it isn't big
+  ${project.height}p
+  ${project.framerate} FPS
+  Platform: ${project.metadata.platform.description}`
 
     const button = document.createElement('button') // make a button
     button.innerText = 'Redownload' // set some text
-    button.addEventListener('click', _ => {
-      const link = document.createElement('a')
-      link.href = URL.createObjectURL(project.file)
-      link.download = project.name + '.wick'
-      link.style.display = 'none'
-      document.body.appendChild(link)
-      link.click()
-      URL.revokeObjectURL(link.href)
-    })
-    document.getElementById('projects').appendChild(info)
+    button.addEventListener('click', project.download.bind(project))
+    holder.appendChild(header)
 
-    document.getElementById('projects').appendChild(button)
+    holder.appendChild(info)
+
+    holder.appendChild(button)
+    document.getElementById('projects').appendChild(holder)
   })
 })
--- worker.mjs
+++
@@ -1,4 +0,0 @@
-/* global addEventListener, postMessage */
-import Project from './wicktools.mjs'
-
-addEventListener('message', async e => postMessage(await new Project(e.data)))
