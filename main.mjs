1259f2576665f356910fd9593946b78fc007a772
Jack <29169102+Jack5079@users.noreply.github.com>
2020-02-07 23:30:27 -0500

    some optimizing

--- main.mjs
+++ main.mjs
@@ -1,58 +1,58 @@
 /* global alert */
 import Project from './wicktools.mjs'
 
-const file = document.getElementById('import')
-function addProjects (array) {
-  array.forEach(async element => {
-    if (
-      element.name.endsWith('.wick') ||
-      element.type === 'application/x-zip-compressed' ||
-      element.type === 'text/html'
-    ) {
-      const project = await new Project(element)
-      console.log(project) // Log it for later
-      // Now, for displaying this data:
-      const holder = document.createElement('article')
-      holder.style.borderColor = project.backgroundColor
-      const header = document.createElement('h3')
-      header.innerText = project.name
-      const res = document.createElement('abbr')
-      res.innerText = project.height + 'p'
-      res.title = `${project.width}x${project.height}`
-      const info = document.createElement('p') // This will have most of the info
-      info.innerText = `${project.framerate} FPS
+async function addProject (proj) {
+  if (
+    proj.name.endsWith('.wick') ||
+    proj.type === 'application/x-zip-compressed' ||
+    proj.type === 'text/html'
+  ) {
+    const project = await new Project(proj)
+    console.log(project) // Log it for later
+    // Now, for displaying this data:
+    const holder = document.createElement('article')
+    holder.style.borderColor = project.backgroundColor
+    const header = document.createElement('h3')
+    header.innerText = project.name
+    const res = document.createElement('abbr')
+    res.innerText = project.height + 'p'
+    res.title = `${project.width}x${project.height}`
+    const info = document.createElement('p') // This will have most of the info
+    info.innerText = `${project.framerate} FPS
   Platform: ${project.metadata.platform.description}`
 
-      const button = document.createElement('button') // make a button
-      button.innerText = '💾' // set some text
-      button.addEventListener('click', project.download.bind(project))
-      holder.appendChild(header)
-      holder.appendChild(res)
-      holder.appendChild(info)
+    const button = document.createElement('button') // make a button
+    button.innerText = '💾' // set some text
+    button.addEventListener('click', project.download.bind(project))
+    holder.appendChild(header)
+    holder.appendChild(res)
+    holder.appendChild(info)
 
-      holder.appendChild(button)
-      document.getElementById('projects').appendChild(holder)
-    } else alert("That's not a project!")
-  })
+    holder.appendChild(button)
+    document.getElementById('projects').appendChild(holder)
+  } else alert("That's not a project!")
 }
-file.addEventListener('change', () => {
+
+document.getElementById('import').addEventListener('change', event => {
   if (document.getElementById('placeholder')) {
     document.getElementById('placeholder').remove()
   }
-  const projects = [...file.files]
-  addProjects(projects)
+  const projects = [...event.currentTarget.files]
+  projects.forEach(addProject)
 })
-document.querySelector('head').addEventListener('dragover', event => {
+
+document.head.addEventListener('dragover', event => {
   event.preventDefault()
   event.dataTransfer.dropEffect = 'none'
 })
 
-document.querySelector('body').addEventListener('drop', event => {
+document.body.addEventListener('drop', event => {
   event.preventDefault()
   const projects = [...event.dataTransfer.files]
-  addProjects(projects)
+  projects.forEach(addProject)
 })
-document.querySelector('body').addEventListener('dragover', event => {
+
+document.body.addEventListener('dragover', event => {
   event.preventDefault()
   event.dataTransfer.dropEffect = 'copy'
 })
